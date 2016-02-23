80e5f09be57d0e5d950ede7673220575e9597abf
ykomitov <yavork@gmail.com>
2016-02-23 16:20:49 +0200

    Removed unnecessary stuff from footer. Fixed styles

---
+++ Web/ArticleSystem.Web/ViewModels/Home/FooterArticlesViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ArticleSystem.Web.ViewModels.Home
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class FooterArticlesViewModel : IMapFrom<Article>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Comments { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Article, FooterArticlesViewModel>()
+                .ForMember(
+                    v => v.Comments,
+                    opt => opt.MapFrom(a => a.Comments.Where(c => c.ArticleId == a.Id).Any() ? a.Comments.Where(c => c.ArticleId == a.Id).Count() : 0));
+        }
+    }
+}
